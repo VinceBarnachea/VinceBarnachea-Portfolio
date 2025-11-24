1033d9841ba5440b175befc22a46ab3ad2757c11
VinceBarnachea <barnacheatolentino@gmail.com>
2025-11-24 15:15:28 +0800

    Try Build Type Via VITE/NPM

---
+++ src/main.js
@@ -0,0 +1,40 @@
+// import './style.css'
+// import javascriptLogo from './javascript.svg'
+// import viteLogo from '/vite.svg'
+// import { setupCounter } from './counter.js'
+
+// // document.querySelector('#app').innerHTML = `
+// //   <div>
+// //     <a href="https://vite.dev" target="_blank">
+// //       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+// //     </a>
+// //     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+// //       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+// //     </a>
+// //     <h1>Hello Vite!</h1>
+// //     <div class="card">
+// //       <button id="counter" type="button"></button>
+// //     </div>
+// //     <p class="read-the-docs">
+// //       Click on the Vite logo to learn more
+// //     </p>
+// //   </div>
+// // `
+
+// // setupCounter(document.querySelector('#counter'))
+import { gsap } from "gsap";
+    
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+// ScrollSmoother requires ScrollTrigger
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+import { SplitText } from "gsap/SplitText";
+
+gsap.registerPlugin(ScrollTrigger,ScrollSmoother,SplitText);
+
+gsap.to('.box',
+  {
+    rotate: 360,
+    duration: 3,
+    scale: 2,
+  }
+);
