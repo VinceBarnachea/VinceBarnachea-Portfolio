afe352dbed2674e7d2f215b182fe8842547a7b1e
VinceBarnachea <barnacheatolentino@gmail.com>
2025-11-24 16:22:12 +0800

    gsap changed to cdn

--- src/main.js
+++ src/main.js
@@ -22,13 +22,6 @@
 // // `
 
 // // setupCounter(document.querySelector('#counter'))
-import { gsap } from "gsap";
-    
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-// ScrollSmoother requires ScrollTrigger
-import { ScrollSmoother } from "gsap/ScrollSmoother";
-import { SplitText } from "gsap/SplitText";
-
 gsap.registerPlugin(ScrollTrigger,ScrollSmoother,SplitText);
 
 gsap.to('.box',
